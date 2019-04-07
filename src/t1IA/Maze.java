fafe03584c98f3f5dd5bb834e458f68fc269184e
Lutt Bitelo <joao.bitelo@acad.pucrs.br>
2019-04-07 00:00:35 -0300

    correcao

--- src/t1IA/Maze.java
+++ src/t1IA/Maze.java
@@ -1,11 +1,19 @@
 package t1IA;
 
 public class Maze {
-	int[][] maze = { { 9, 0, 0, 0, 0, 0, 0, 0, 1, 0 }, { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-			{ 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 }, { 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 0, 1, 0, 0, 1, 0, 0, 0 },
-			{ 1, 1, 1, 1, 0, 1, 1, 1, 1, 1 }, { 1, 0, 0, 0, 0, 1, 1, 0, 1, 8 }, { 1, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
-			{ 1, 0, 0, 0, 0, 0, 0, 1, 1, 0 }, { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }, };
-
+	int[][] maze = { { 9, 0, 0, 0, 0, 0, 0, 0, 1, 0 }, 
+					 { 2, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+					 { 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 }, 
+					 { 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 }, 
+					 { 1, 1, 0, 1, 0, 0, 0, 0, 0, 0 },
+					 { 1, 1, 1, 1, 0, 1, 1, 1, 1, 1 }, 
+					 { 1, 0, 0, 0, 0, 1, 1, 0, 1, 8 }, 
+					 { 1, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
+					 { 1, 0, 0, 0, 0, 0, 0, 1, 1, 0 }, 
+					 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }, };
+	
+	private int entrada[][];
+	private int saida[][];
 	public Maze() {
 
 	}
@@ -17,6 +25,22 @@ public class Maze {
 	public int[][] getMatriz() {
 		return maze;
 
+	}
+
+	public int[][] getEntrada() {
+		return entrada;
+	}
+
+	public void setEntrada(int entrada[][]) {
+		this.entrada = entrada;
+	}
+
+	public int[][] getSaida() {
+		return saida;
+	}
+
+	public void setSaida(int saida[][]) {
+		this.saida = saida;
 	}
 
 }
--- src/t1IA/Tela.java
+++ src/t1IA/Tela.java
@@ -44,10 +44,10 @@ public class Tela extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 
-		JButton btnNewButton = new JButton("Iniciar");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		JButton ButtonAlgoritmoA = new JButton("Iniciar");
+		ButtonAlgoritmoA.setFont(new Font("Tahoma", Font.PLAIN, 18));
 
-		JLabel lblAlgoritmoA = new JLabel("Algoritmo A*");
+		JLabel lblAlgoritmoA = new JLabel("Algoritmo Gen\u00E9tico");
 		lblAlgoritmoA.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAlgoritmoA.setFont(new Font("Tahoma", Font.PLAIN, 21));
 		
@@ -56,37 +56,53 @@ public class Tela extends JFrame {
 		JPanel panel_4 = new JPanel();
 		panel_4.setBackground(Color.black);
 		
+		JLabel lblAlgoritmoGentico = new JLabel("Algoritmo A*");
+		lblAlgoritmoGentico.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAlgoritmoGentico.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		
+		JButton ButtonAlgoritmoGnetico = new JButton("Iniciar");
+		ButtonAlgoritmoGnetico.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
 						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(panel, GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addComponent(ButtonAlgoritmoGnetico, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(lblAlgoritmoA, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE))
+									.addGap(29)))
+							.addPreferredGap(ComponentPlacement.RELATED)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(panel, GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE)
-								.addComponent(lblAlgoritmoA, GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE)
-								.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE))))
+								.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
+								.addComponent(ButtonAlgoritmoA, GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
+								.addComponent(lblAlgoritmoGentico, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE))))
 					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblAlgoritmoA)
-						.addComponent(btnNewButton))
+						.addComponent(lblAlgoritmoGentico, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblAlgoritmoA))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(ButtonAlgoritmoA)
+						.addComponent(ButtonAlgoritmoGnetico, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addGap(10)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGroup(Alignment.TRAILING, gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 367, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
 							.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 367, Short.MAX_VALUE)
 							.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 367, Short.MAX_VALUE)))
 					.addContainerGap())
@@ -94,9 +110,9 @@ public class Tela extends JFrame {
 		panel_1.setLayout(new BorderLayout(0, 0));
 		panel.setLayout(new BorderLayout(0, 0));
 
-		panel.add(new DrawEstrela(), BorderLayout.CENTER);
 		contentPane.setLayout(gl_contentPane);
-		btnNewButton.addActionListener(new ButtonListener());
+		ButtonAlgoritmoA.addActionListener(new ButtonListener());
+		ButtonAlgoritmoGnetico.addActionListener(new ButtonListener2());
 		
 
 	}
@@ -135,8 +151,18 @@ public class Tela extends JFrame {
     {
         public void actionPerformed(ActionEvent e)
         {
-        	panel_1.add(new DrawHeuristica(), BorderLayout.CENTER);
+        	panel_1.add(new DrawEstrela(), BorderLayout.CENTER);
     		setVisible(true);       	 
+        }
+    }
+	public class ButtonListener2 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+        	panel.add(new DrawHeuristica(), BorderLayout.CENTER);
+    		setVisible(true); 
+    		
+    		
         }
     }
 }
